c8358e6d4b4b3aa7088a6fe5114746ce44879d98
Shawn Wang <wang77289271@gmail.com>
2022-08-31 13:28:00 -0600

    fetch repository data from the Express API

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,8 +3,24 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ReposList from './components/ReposList';
 import Repo from './components/Repo';
 import ErrorPage from './components/ErrorPage';
+import { useEffect, useState } from 'react';
 
 export function App() {
+  const [repos, setRepos] = useState([]);
+
+  // fetch repository data from 'http://localhost:4000/repos'
+  const url = 'http://localhost:4000/repos';
+  const getData = async () => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setRepos(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  console.log(repos);
+
   return (
     <div className="App">
       <Router>
